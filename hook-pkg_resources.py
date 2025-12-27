cd8c490011146970bd547893d579558c217bce9a
NH5 <NH5wuzheng@outlook.com>
2025-12-27 22:20:33 +0800

    Explicitly include jaraco modules in requirements and hook

--- hook-pkg_resources.py
+++ hook-pkg_resources.py
@@ -1,3 +1,10 @@
-from PyInstaller.utils.hooks import collect_submodules
+from PyInstaller.utils.hooks import collect_submodules, copy_metadata
 
-hiddenimports = collect_submodules('jaraco')
+hiddenimports = collect_submodules('jaraco') + [
+    'jaraco.text',
+    'jaraco.context',
+    'jaraco.functools',
+    'jaraco.classes',
+]
+
+datas = copy_metadata('jaraco.text')
